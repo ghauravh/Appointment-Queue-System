049f5a3609b47cf6de3f2d52e3d26940d7268000
ghauravh <aghauravh@gmail.com>
2026-02-01 00:10:41 +0530

    changed the design of the calendar

--- appointment-scheduler-frontend/src/components/Header.js
+++ appointment-scheduler-frontend/src/components/Header.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import './Header.css';
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="header-content">
-                <div className="logo">
-                    <h1>MedLife</h1>
-                </div>
-                <div className="tagline">
-                    <p>We care for better lives</p>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <h1 className="clinic-name">NovaCare Clinic</h1>
+      <p className="tagline">Care you can trust</p>
+    </header>
+  );
 };
 
 export default Header;
