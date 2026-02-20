4476f57d2f041922fb86d97b06fdeb04878b4273
ghauravh <aghauravh@gmail.com>
2026-02-20 22:00:22 +0530

    changing design of dashboard

--- appointment-scheduler-frontend/src/App.js
+++ appointment-scheduler-frontend/src/App.js
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import LoginPage from "./pages/LoginPage";
 import AppointmentForm from "./components/AppointmentForm";
+import AdminDashboard from "./components/AdminDashboard";
+import PatientDashboard from "./components/PatientDashboard";
 import "./App.css";
 
-import PatientDashboard from "./components/PatientDashboard";
-
 function App() {
-  return <PatientDashboard />;
+  const [user, setUser] = useState(null);  // stores logged in user
+  const [role, setRole] = useState(null);  // "patient" or "admin"
+
+  if (!user) {
+    return (
+      <LoginPage
+        onLogin={(userData) => {
+          setUser(userData);
+
+          // Simple role logic (temporary)
+          if (userData.email === "admin@clinic.com") {
+            setRole("admin");
+          } else {
+            setRole("patient");
+          }
+        }}
+      />
+    );
+  }
+
+  return (
+    <>
+      {role === "admin" && <AdminDashboard />}
+      {role === "patient" && <AppointmentForm />}
+    </>
+  );
 }
 
 export default App;
